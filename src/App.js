3569e9a1b154638938499fba471ccbe3466841b0
choihuk <love72huk@naver.com>
2024-12-25 13:57:40 +0900

    버튼 사라지지 않도록 변경

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css'; // Import custom CSS for styling
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS for UI components
+import './App.css'; // Custom CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
 import SongSelection from './components/SongSelection';
-import WeeklyPlaylist from './components/WeeklyPlaylist'
+import WeeklyPlaylist from './components/WeeklyPlaylist';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-light bg-light">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
               성가 서비스
             </Link>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    성가 유튜브 청취 서비스
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/select">
-                    성가 선정 서비스
-                  </Link>
-                </li>
-              </ul>
+            {/* Navigation Buttons */}
+            <div className="d-flex">
+              <Link className="btn btn-outline-primary me-2" to="/">
+                성가 유튜브 청취 서비스
+              </Link>
+              <Link className="btn btn-outline-secondary" to="/select">
+                성가 선정 서비스
+              </Link>
             </div>
           </div>
         </nav>
